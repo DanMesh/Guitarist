a86e1244a5a3a3ca9253ee405dffe4698d009207
Daniel Mesham <danielmesham@gmail.com>
2018-04-13 18:03:34 +0200

    Automated audio file fetching

--- matlab/Guitarist.m
+++ matlab/Guitarist.m
@@ -4,19 +4,26 @@ close all;
 addpath('./yin/');
 addpath('./midi/');
 
-files = [string('E2_6.m4a'), string('E3_4.m4a'), string('E4_1.m4a')];
-files2 = 
+dataDir = '../data/';
 
-notes = string.empty(0,3);
-stddev = zeros(1,3);
-error = zeros(1,3);
+D = dir(fullfile(dataDir, '*.m4a'));    % Get audio contents of data directory
+N = length(D);                          % Number of files
+files = string.empty(0,N);              % List of file paths
+
+for i = 1:N
+    files(i) = strcat(dataDir, D(i).name);
+end
+
+notes = string.empty(0,N);
+stddev = zeros(1,N);
+error = zeros(1,N);
 
 %[y, Fs] = getAudio(files);
 
 fprintf('\n%-8s %-8s %-8s \n', 'TRUTH', 'EST', 'STD DEV');
 disp('----------------------------');
 
-for i = 1:length(files)
+for i = 1:N
     [y, Fs] = getAudio(files(i));
     periods = getPeriod(y, Fs);
     T = mean(periods);
@@ -25,11 +32,13 @@ for i = 1:length(files)
     
     % Analyse results
     splt = strsplit(files(i), '_');
-    truth = splt(1);
+    truth = erase(splt(1), dataDir);
     stddev(i) = std(periods)/T;
     error(i) = truth ~= notes(i);
     fprintf('%-8s %-8s %.6f \n', truth, notes(i), stddev(i));
 end
 
+fprintf('\n');
 fprintf('Worst case std dev = %.6f \n', max(stddev));
-fprintf('Errors = %d \n', sum(error));
+fprintf('Errors = %d / %d \n', sum(error), N);
+fprintf('\n');
--- matlab/getAudio.m
+++ matlab/getAudio.m
@@ -6,9 +6,10 @@ function [y, Fs] = getAudio(filename)
 %GETAUDIO Converts an audio file into a vector of signal values
 %   Returns a tuple with the signal and the sampling frequency
 
-if ~exists('filename', 'var')
-    filename = 'E2_6.m4a';      % Default audio
-end
+%if ~exists('filename', 'var')
+%    filename = 'E2_6.m4a';      % Default audio
+%end
 
-[y, Fs] = audioread(strcat('../data/', filename));
+path = char(filename);
+[y, Fs] = audioread(path);
 end
